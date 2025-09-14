da68b94251dff54c6699df94a70c0c5a1dc320dd
pedro henrique <pedro.henrike2812@gmail.com>
2025-09-14 11:28:05 -0300

    feat: criando api wrapper

---
+++ src/env.ts
@@ -0,0 +1,17 @@
+import { z } from 'zod'
+
+const envSchema = z.object({
+  NEXT_PUBLIC_API_BASE_URL: z.url(),
+
+})
+
+const parseSchema = envSchema.safeParse(process.env)
+
+if(!parseSchema.success) {
+  
+  console.error("Invalid enviroment variables", z.treeifyError(parseSchema.error))
+
+  throw new Error("Invalid enviroment variables")
+}
+
+export const env = parseSchema.data
