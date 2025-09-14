6b92237f50516508eb841636d4d46f32e9742d7b
pedro henrique <pedro.henrike2812@gmail.com>
2025-09-14 11:02:39 -0300

    feat: criando route handler

---
+++ src/app/api/products/featured/route.ts
@@ -0,0 +1,7 @@
+import data from '../data.json'
+
+export function GET() {
+  const featuredProducts = data.products.filter((product) => product.featured)
+
+  return Response.json(featuredProducts)
+}
