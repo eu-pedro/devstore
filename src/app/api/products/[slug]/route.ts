517ef08722e514feeddcd33ec4e13846bc679888
pedro henrique <pedro.henrike2812@gmail.com>
2025-09-14 18:26:51 -0300

    feat: fetch de dados

---
+++ src/app/api/products/[slug]/route.ts
@@ -0,0 +1,22 @@
+import { z } from 'zod'
+
+import data from '../data.json'
+
+export async function GET(
+  _: Request,
+  { params }: { params: { slug: string } },
+) {
+  await new Promise((resolve) => setTimeout(resolve, 2000))
+
+  const { slug } = await params
+
+  const slugProduct = z.string().parse(slug)
+
+  const product = data.products.find((product) => product.slug === slugProduct)
+
+  if (!product) {
+    return Response.json({ message: 'Product not found.' }, { status: 400 })
+  }
+
+  return Response.json(product)
+}
