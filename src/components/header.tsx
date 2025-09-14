3bb43d7b2b98eaa554b9c1ea67496eb7dc8ad15b
pedro henrique <pedro.henrike2812@gmail.com>
2025-09-14 20:17:59 -0300

    feat: loading de busca

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,35 +1,40 @@
 import Link from "next/link";
-import { Search } from 'lucide-react';
 import Image from "next/image";
 import { CartWidget } from "./cart-widget";
+import { Suspense } from "react";
+import { SearchForm } from "./search-form";
 
 export function Header() {
 
 
   return (
-    <header className="flex items-center justify-between">
+   <header className="flex items-center justify-between">
       <div className="flex items-center gap-5">
-        <Link href="/" className="text-2xl font-extrabold text-white">devstore</Link>
+        <Link href="/" className="text-2xl font-extrabold text-white">
+          devstore
+        </Link>
 
-        <form className="flex w-[320px] items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700">
-          <Search className="w-5 h-5 text-zinc-500" />
-
-          <input type="text" placeholder="Buscar produtos..." className="flex-1 bg-transparent text-sm outline-none placeholder:text-zinc-500" />
-        </form>
+        <Suspense fallback={null}>
+          <SearchForm />
+        </Suspense>
       </div>
+
       <div className="flex items-center gap-4">
         <CartWidget />
 
-        <div className="w-px h-4 bg-zinc-700" />
-
+        <div className="w-px h-4 bg-zinc-700"></div>
 
         <Link href="/" className="flex items-center gap-2 hover:underline">
           <span className="text-sm">Account</span>
-          <Image src="https://github.com/eu-pedro.png" className="h-6 w-6 rounded-full" width={24} height={24} alt="" />
-
+          <Image
+            src="https://github.com/diego3g.png"
+            className="h-6 w-6 rounded-full"
+            width={24}
+            height={24}
+            alt=""
+          />
         </Link>
       </div>
-
     </header>
   )
 }
